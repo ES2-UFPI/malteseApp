86e8804823e2d0f6f4755a26a7f02971cb164015
Wenderson <wps713@gmail.com>
2020-12-12 20:12:49 -0300

    add initial store card #1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 import { Container, Title } from './styles';
 
+import { Card } from '~/components/theme';
+
 const Home = () => {
   return (
     <Container>
-      <Title>Welcome to maltese</Title>
+      <Title primaryFont>Welcome to maltese</Title>
+      <Card
+        title="Amherst"
+        subtitle="The best artesanal brew in town"
+        imageSource={{
+          uri:
+            'http://theworthybrewfest.com/worthy/wp-content/uploads/2015/06/Amherst-Brewing-Company-logo.png',
+        }}
+      />
     </Container>
   );
 };
