46ef818d79e4c98b2758716da652023417a13959
Wenderson <wps713@gmail.com>
2020-12-12 10:12:58 -0300

    setting up initial project

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from '~/pages/Home';
+
+const Tab = createBottomTabNavigator();
+
+export default function Routes() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+    </Tab.Navigator>
+  );
+}
