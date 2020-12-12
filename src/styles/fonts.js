1a6da926a35f60e200771313124a2eefc2c77017
Wenderson <wps713@gmail.com>
2020-12-12 17:51:11 -0300

    setting up custom fonts in app

---
+++ src/styles/fonts.js
@@ -0,0 +1,16 @@
+export const fonts = {
+  primary: {
+    light: 'K2D-Light',
+    regular: 'K2D-Regular',
+    medium: 'K2D-Medium',
+    semiBold: 'K2D-SemiBold',
+    bold: 'K2D-Bold',
+  },
+  secondary: {
+    light: 'Montserrat-Light',
+    regular: 'Montserrat-Regular',
+    medium: 'Montserrat-Medium',
+    semiBold: 'Montserrat-SemiBold',
+    bold: 'K2D-Bold',
+  },
+};
