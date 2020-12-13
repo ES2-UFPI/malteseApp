0d93a70629231cc6c0d1d9d89b45f6396d5aeb23
Wenderson <wps713@gmail.com>
2020-12-13 20:45:47 -0300

    add usePrimarycolor prop in Icon #1

--- src/components/global/Icon/index.js
+++ src/components/global/Icon/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useTheme } from 'styled-components';
 
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
-const Icon = ({ size = 18, color = '#fff', name = 'rocket' }) => {
-  return <Ionicons size={size} color={color} name={name} />;
+const Icon = ({ size = 18, color, usePrimaryColor, name = 'rocket' }) => {
+  const theme = useTheme();
+
+  return (
+    <Ionicons
+      size={size}
+      color={usePrimaryColor ? theme.colors.primary : color}
+      name={name}
+    />
+  );
 };
 
 export default Icon;
