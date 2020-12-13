5f425a39dd163107aab25a78d02d629033e66e07
Wenderson <wps713@gmail.com>
2020-12-13 19:03:04 -0300

    changing store tab navigation to stack #1

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from '~/pages/Home';
 import Store from '~/pages/Store';
 
@@ -7,8 +8,30 @@ import { Icon } from '~/components/global';
 import colors from '~/constants/colors';
 
 const Tab = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
 
 export default function Routes() {
+  function HomeStackScreen() {
+    return (
+      <HomeStack.Navigator>
+        <HomeStack.Screen name="Home" component={Home} />
+        <HomeStack.Screen
+          name="Store"
+          component={Store}
+          options={({ route }) => ({
+            title: route.params.storeName,
+            headerTitleStyle: {
+              textAlign: 'center',
+              alignSelf: 'center',
+              fontFamily: 'K2D-Regular',
+            },
+            headerLeft: false,
+          })}
+        />
+      </HomeStack.Navigator>
+    );
+  }
+
   return (
     <Tab.Navigator
       screenOptions={({ route }) => ({
@@ -26,8 +49,7 @@ export default function Routes() {
         inactiveTintColor: colors.dark,
       }}
     >
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Store" component={Store} />
+      <Tab.Screen name="Home" component={HomeStackScreen} />
     </Tab.Navigator>
   );
 }
