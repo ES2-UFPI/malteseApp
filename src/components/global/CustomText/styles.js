f92b6d126f1dd4f2d66fe24c2a8e7e5a312c5a17
Wenderson <wps713@gmail.com>
2020-12-16 12:30:27 -0300

    adding dark theme color in custom text

--- src/components/global/CustomText/styles.js
+++ src/components/global/CustomText/styles.js
@@ -3,4 +3,5 @@ import styled from 'styled-components/native';
 export const Text = styled.Text`
   font-family: ${({ theme, primaryFont }) =>
     primaryFont ? theme.fonts.primary.regular : theme.fonts.secondary.regular};
+  color: ${({ theme }) => theme.colors.dark};
 `;
