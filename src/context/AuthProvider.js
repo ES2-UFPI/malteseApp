fcf0498478a7f5c99c0ab6664ec8172c45c843f9
wenderson-p <wps713@gmail.com>
2021-01-17 09:19:16 -0300

    add mocked data for each user

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -5,9 +5,39 @@ export const AuthContext = createContext({});
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  const getMockedData = type => {
+    switch (type) {
+      case 'client':
+        return {
+          _id: '5fff9741b9aa894028ac3fe1',
+          name: 'João',
+          user: '5fe0021ddba9cd1984b3cfc6',
+          createdAt: '2021-01-14T00:58:41.612Z',
+        };
+      case 'provider':
+        return {
+          _id: '5fff97b5b9aa894028ac3fe2',
+          name: 'JoãoBAR',
+          user: '5fe0021ddba9cd1984b3cfc6',
+          products: [],
+          createdAt: '2021-01-14T01:00:37.285Z',
+        };
+      case 'deliverer':
+        return {
+          _id: '5ffffba315eb082ff0debc64',
+          name: 'JoãoENTREGAS',
+          user: '5fe0021ddba9cd1984b3cfc6',
+          createdAt: '2021-01-14T08:06:59.477Z',
+        };
+      default:
+        break;
+    }
+  };
+
   const handleLogin = type => {
     // Dados mockados de 3 users aqui
-    setUser({ name: 'João', type });
+    const mockedData = getMockedData(type);
+    setUser({ ...mockedData, type });
   };
 
   return (
