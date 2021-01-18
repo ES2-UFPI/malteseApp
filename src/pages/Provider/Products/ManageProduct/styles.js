4d1d9550ac6f34e1a3acc56eddc4e566dde8c9e8
wenderson-p <wps713@gmail.com>
2021-01-17 22:25:09 -0300

    add manage product page

---
+++ src/pages/Provider/Products/ManageProduct/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components/native';
+import { CustomText } from '~/components/global';
+
+export const Container = styled.View`
+  height: 100%;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderContainer = styled.View`
+  width: 100%;
+  justify-content: space-between;
+`;
+
+export const DeleteButton = styled.TouchableOpacity``;
+
+export const Title = styled(CustomText)`
+  margin: 16px;
+  font-size: 22px;
+`;
+
+export const Input = styled.TextInput`
+  text-align: center;
+  width: 80%;
+  margin: 16px;
+  padding: 8px;
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.colors.gray};
+  font-size: 16px;
+`;
