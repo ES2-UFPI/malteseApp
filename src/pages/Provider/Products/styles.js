e96ae69ef18a51561f322ee9461503398db9dd86
wenderson-p <wps713@gmail.com>
2021-01-17 22:23:41 -0300

    add product page

---
+++ src/pages/Provider/Products/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import { CustomText } from '~/components/global';
+
+export const Container = styled.View``;
+
+export const ProductsList = styled.FlatList``;
+
+export const Title = styled(CustomText)``;
