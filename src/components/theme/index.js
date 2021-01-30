f53aba9886fa5d2e22d7453eb76928e08e9a7204
wenderson-p <wps713@gmail.com>
2021-01-30 20:35:36 -0300

    exporting new components

--- src/components/theme/index.js
+++ src/components/theme/index.js
@@ -1,4 +1,7 @@
 export { default as Button } from './Button';
 export { default as Card } from './Card';
 export { default as Modal } from './Modal';
+export { default as OrderItemsList } from './OrderItemsList';
+export { default as OrderStatusContainer } from './OrderStatusContainer';
 export { default as ProductCard } from './ProductCard';
+export { default as StarsRating } from './StarsRating';
