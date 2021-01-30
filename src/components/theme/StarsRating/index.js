cd05e79f0ca34eb394ca7cd6e9f098b1767ed741
wenderson-p <wps713@gmail.com>
2021-01-30 20:33:31 -0300

    add stars rating component

---
+++ src/components/theme/StarsRating/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Icon from '../../global/Icon';
+import { StarButton, StarContainer } from './styles';
+import colors from '~/constants/colors';
+
+const StarsRating = ({ isButton = false, selectedStars, changeRating }) => {
+  const stars = [1, 2, 3, 4, 5];
+
+  return (
+    <StarContainer>
+      {stars.map(index => (
+        <StarButton onPress={() => changeRating(index)} disabled={!isButton}>
+          <Icon
+            name="star"
+            size={24}
+            color={index <= selectedStars ? colors.primary : colors.gray}
+          />
+        </StarButton>
+      ))}
+    </StarContainer>
+  );
+};
+
+export default StarsRating;
